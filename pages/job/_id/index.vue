96870f3053100344a11115f14ccead743fb5e406
Kelvin Graddick <kelvingraddick@kg.codes>
2021-08-18 02:55:14 -0400

    Job delete button and job apply button

--- pages/job/_id/index.vue
+++ pages/job/_id/index.vue
@@ -10,29 +10,22 @@
               ></v-img>
               <v-card-title class="text-h5 font-weight-bold">{{job && job.title}}</v-card-title>
               <v-card-text>
-                <div class="h6 font-weight-bold">{{job && job.type}}</div>
-                <div></div>
+                <div class="h6 font-weight-bold">{{job && job.type}} &middot; {{job && job.city}}, {{job && job.state}}</div>
+                <div>{{job && job.employer && job.employer.name}}</div>
               </v-card-text>
               <v-divider class="mx-4"></v-divider>
               <v-card-actions>
                 <v-btn
                   color="primary"
                   text
-                  @click="onLinkedInButtonClick"
+                  @click="onApplyButtonClick"
                 >
                   <v-icon>mdi-email</v-icon>&nbsp;Apply
-                </v-btn>
-                <v-btn
-                  color="primary"
-                  text
-                  @click="onLinkedInButtonClick"
-                >
-                  <v-icon>mdi-linkedin</v-icon>&nbsp;LinkedIn
                 </v-btn>
                  <v-btn
                   color="primary"
                   text
-                  @click="onLinkedInButtonClick"
+                  @click="onShareButtonClick"
                 >
                   <v-icon>mdi-file-document</v-icon>&nbsp;Share
                 </v-btn>
@@ -48,12 +41,21 @@
               <v-card-subtitle class="">{{job && job.description}}</v-card-subtitle>
               <v-card-title class="">Instructions</v-card-title>
               <v-card-subtitle class="">{{job && job.instructions}}</v-card-subtitle>
-              <v-card-title class="">City</v-card-title>
-              <v-card-subtitle class="">{{job && job.city}}</v-card-subtitle>
-              <v-btn depressed @click="onApplyButtonClick" class="font-weight-bold ma-4" color="error" height="40">
-                Apply &nbsp;
-                <v-icon>mdi-form</v-icon>
-              </v-btn>
+              <v-card-actions v-show="!isProcessing" class="ma-2">
+                <v-btn depressed @click="onApplyButtonClick" class="font-weight-bold" color="primary" height="40">
+                  Apply &nbsp;
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn v-if="isJobOwner" depressed @click="onDeleteButtonClick" class="font-weight-bold" color="error" height="40">
+                  Delete &nbsp;
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-progress-linear
+                indeterminate
+                color="error"
+                v-show="isProcessing"
+              ></v-progress-linear>
             </v-card>
           </v-col>
         </v-row>
@@ -71,14 +73,19 @@
     data: function() {
       return {
         content: Content,
-        job: undefined
+        job: undefined,
+        isProcessing: false
       }
     },
     computed: {
       userImageUrl: function() {
         var storedJob = this.$store.state.job;
         return (storedJob && storedJob.imageUrl) || '/placeholder-user.png';
-      }
+      },
+      isJobOwner: function() {
+        var storedEmployer = this.$store.state.employer;
+        return storedEmployer && storedEmployer._id == this.job?.employer._id;
+      },
     },
     mounted: function() {
       Authorize.tryEmployerSignIn(this.$store);
@@ -103,10 +110,47 @@
 		},
     methods: {
       async onApplyButtonClick() {
+        window.open(this.job?.postingUrl, '_blank');
+      },
+      async onShareButtonClick() {
 
       },
-      async onLinkedInButtonClick() {
-
+      async onDeleteButtonClick() {
+        this.isProcessing = true;
+        var response = await this.delete();
+        if (response) {
+          if (response.isSuccess) {
+            // success
+            this.$router.push({ path: '/employer/' + this.$store.state.employer._id });
+          } else {
+            window.alert(response.errorMessage);
+            this.isProcessing = false;
+          }
+        } else {
+          var errorMessage = "There was an error deleting the job." + " Please update entries and try again";
+          window.alert(errorMessage);
+          this.isProcessing = false;
+        }
+      },
+      async delete() {
+        return fetch('http://api.educateme.wavelinkllc.com/job/delete/' + this.job._id, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + this.$store.state.token },
+        })
+        .then((response) => { 
+          if (response.status == 200) {
+            return response.json()
+            .then((responseJson) => {
+              return responseJson;
+            })
+          } else {
+            return undefined;
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          return undefined;
+        });
       }
     }
   }
