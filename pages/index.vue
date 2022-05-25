ab508b3b585008505aab50c56e3fb8ad65555342
Kelvin Graddick <kelvingraddick@kg.codes>
2022-05-25 03:28:51 -0400

    Coming soon page (2)

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,21 @@
   <v-container>
     <div class="text-h4 font-weight-bold mt-5">
       EDCOM HQ Jobs is coming soon!
+      <div class="red-bar"></div>
     </div>
     <v-layout class="mt-10 mb-10" row wrap>
       <v-flex xs12 md8>
-        <div>
-          <v-card>
-            <div class="video">
-              <iframe src="https://www.youtube.com/embed/7uUGBgCfEKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <v-card-title style="display: block;" primary-title>
-              <div class="text-h4 font-weight-bold">
-                {{content['About Section Title Text']}}
-              </div>
-              <div class="red-bar"></div>
-              <div style="word-break: normal;" class="text-h5 font-weight-medium mt-3">
-                {{content['About Section Description Text']}}
-              </div>
-            </v-card-title>
-          </v-card>
+        <div class="video">
+          <iframe src="https://www.youtube.com/embed/7uUGBgCfEKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
+        <v-card class="mt-10">
+          <v-card-title style="display: block;" primary-title>
+            <div style="word-break: normal;" class="text-h5 font-weight-medium">
+              {{content['About Section Description Text']}}
+            </div>
+            <div class="red-bar mt-3"></div>
+          </v-card-title>
+        </v-card>
       </v-flex>
       <v-flex xs0 md4>
 
